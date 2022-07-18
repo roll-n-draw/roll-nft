a3cd8cac9eb061b73b7e67cf1f7896da3fbdc214
box <0xb0x@protonmail.com>
2022-07-18 20:12:34 +0100

    feat: initial Raffle Contract

--- contract/contracts/RaffleUtils.sol
+++ contract/contracts/RaffleUtils.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 library RandomUtils {
 
     function range(uint256 x) external returns(uint256[] memory){}
-    function shuffle() external returns(uint256[] memory)
+    function shuffle() external returns(uint256[] memory) {}
 }
