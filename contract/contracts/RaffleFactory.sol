a9498fd6176d2bb94bf74eba9687845c2b5c94bf
0xb0x <0xb0x@protonmail.com>
2022-07-21 21:11:09 +0100

    update logic, fix bugs

--- contract/contracts/RaffleFactory.sol
+++ contract/contracts/RaffleFactory.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 import {ClonesWithImmutableArgs} from "clones-with-immutable-args/ClonesWithImmutableArgs.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./Raffle.sol";
-import "./IPrize.sol";
 import "./LinkTokenInterface.sol";
 
 contract RaffleFactory {
      using ClonesWithImmutableArgs for address;
 
     /// @dev current raffle Id
-    uint256 internal currentRaffleId;
+    address[] public allRaffles;
     /// @dev raffle implementation contract
     address internal immutable implementation;
     /// @dev link token contract address
@@ -24,21 +24,18 @@ contract RaffleFactory {
     address public owner;
     /// @dev fee percentage i.e 1%(100/10000)
     uint256 public feePercent;
-    /// @dev mapping of raffle Id to address
-    mapping (uint256 => address) public rollIDToAddress;
 
     /// EVENTS
-    event RaffleCreated(uint256 indexed raffleID, address indexed hostAddress, uint256 mintPrice, uint256 deadline);
+    event RaffleCreated(address indexed raffleCreated, address indexed hostAddress, uint256 mintPrice, uint256 deadline);
     event FeeSet(uint256 newFee);
 
 
 
-    constructor(address _vrfCoordinator, address linkToken, uint64 _subscriptionId, bytes32 _keyHash) {
+    constructor(address _vrfCoordinator, address linkToken, bytes32 _keyHash) {
+        owner = msg.sender;
         implementation = address(new Raffle());
         VRF_COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
         LINK_TOKEN = LinkTokenInterface(linkToken);
-        owner = msg.sender;
-        subscriptionId = _subscriptionId;
         KEY_HASH = _keyHash;
     }
 
@@ -50,9 +47,9 @@ contract RaffleFactory {
         uint256 _entryUpperLimit,
         uint256 _entryLowerLimit,
         uint256 _mintCost,
-        IPrize.Prize[] calldata prizes
+        IERC721 _prizeAddress,
+        uint256 _prizeId
     ) external returns(Raffle raffle){
-        uint256 _currentRaffleId = currentRaffleId;
         bytes memory data = abi.encodePacked(
             KEY_HASH,
             subscriptionId,
@@ -69,22 +66,26 @@ contract RaffleFactory {
         raffle = Raffle(implementation.clone(data));
         //initialize raffle contract
         raffle.initialize(
-            string(abi.encodePacked("RAFFLE__",currentRaffleId)),
-            string(abi.encodePacked("RFL__",currentRaffleId)),
-            prizes,
+            string(abi.encodePacked("RAFFLE__",address(raffle))),
+            string(abi.encodePacked("RFL__",address(raffle))),
+            _prizeAddress, 
+            _prizeId,
             msg.sender,
             address(VRF_COORDINATOR)
         );
 
-        rollIDToAddress[_currentRaffleId] = address(raffle);
-        // transfer raffle prize to raffle contract
-        for(uint256 i=0; i < prizes.length;){
-            prizes[i].prizeAddress.transferFrom(msg.sender, address(raffle), prizes[i].prizeId);
-            unchecked{i++;}
-        }
+        allRaffles.push(address(raffle));
+        
+        // transfer prize to factory
+        _prizeAddress.transferFrom(msg.sender, address(this), _prizeId);
+
+        // approve and transfer raffle prize to raffle contract
+        _prizeAddress.approve(address(raffle), _prizeId);
+        _prizeAddress.transferFrom(address(this), address(raffle), _prizeId);
+
         addConsumer(address(raffle));
-        currentRaffleId = _currentRaffleId++;
-        emit RaffleCreated(_currentRaffleId, msg.sender, _mintCost, _endTime);
+
+        emit RaffleCreated(address(raffle), msg.sender, _mintCost, _endTime);
 
 
     }
@@ -115,6 +116,7 @@ contract RaffleFactory {
         if(owner != msg.sender){ 
             revert();
         }
+        LINK_TOKEN.approve(address(VRF_COORDINATOR), type(uint).max);
         LINK_TOKEN.transferAndCall(address(VRF_COORDINATOR), amount, abi.encode(subscriptionId));
     }
 
